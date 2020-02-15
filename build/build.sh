4319f04df151e4806ac87d9861f56e4275dbd679
Gus Parvin <gparvin@us.ibm.com>
2020-02-15 12:15:57 -0500

    define variables for helm build

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export HELM_PACKAGE_PATH=stable/ibm-cert-manager-webhook
+export CHART_NAME=$(cat $TRAVIS_BUILD_PATH/COMPONENT_NAME 2>/dev/null)
+export TARGET_VERSION=$(cat $TRAVIS_BUILD_PATH/COMPONENT_VERSION 2>/dev/null)
+make init
 make helm/chart/build/package
